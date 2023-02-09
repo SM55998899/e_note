874ff85648f9e55482f9129cc70906800183cad2
sakamino <shotapekka@gmail.com>
2023-02-09 15:03:39 +0900

    modeling-users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root "static_pages#home"
   get '/help', to: "static_pages#help"
   get '/search', to: 'searchs#search'
+  get '/signup', to: 'users#new'
 
 end
