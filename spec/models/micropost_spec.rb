f3ce691ffe8251c8f52a354ddbadb927bcc1253d
sakamino <shotapekka@gmail.com>
2023-02-24 00:13:26 +0900

    マイクロポストの基本的なテストが完了。これから日記投稿フォームを制作開始

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Micropost, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+# associationで関連付けしているので、Userオブジェクトを自分で作らなくていい!!
+  let(:micropost) { FactoryBot.create(:micropost) }
+
+  it "投稿テストデータが有効か" do
+    expect(micropost).to be_valid
+  end
+
+  it "ユーザidがないと無効になるか" do
+    micropost.user_id = nil
+    expect(micropost).to be_invalid
+  end
+
+  it "コンテンツがないと無効になるか" do
+    micropost.content = " "
+    expect(micropost).to be_invalid
+  end
+
+  it "投稿が140字以内かどうか" do
+    micropost.content = "a" * 141
+    expect(micropost).to be_invalid
+  end
+
+  describe "Sort by latest" do
+    # 評価される前にdbに保存されていないといけないので、「let!」を使用します。
+       let!(:day_before_yesterday) { FactoryBot.create(:micropost, :day_before_yesterday) }
+    # FactoryBot.create(:micropost, :now) を一番上に持ってくるとテストの意味がなくなるので注意です。
+       let!(:now) { FactoryBot.create(:micropost, :now) }
+       let!(:yesterday) { FactoryBot.create(:micropost, :yesterday) }
+   
+       it "succeeds" do
+         expect(Micropost.first).to eq now
+       end
+     end
 end
