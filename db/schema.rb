5a24f53084fd089884b541738f1af9e3eecaaf94
sakamino <shotapekka@gmail.com>
2023-02-23 22:45:04 +0900

    ユーザプロフィールが表示できないバグを修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_19_052732) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_23_133359) do
   create_table "microposts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id", null: false
