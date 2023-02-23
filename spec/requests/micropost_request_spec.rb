6bc6590f797c773102e6504fcbacff78d61a48e2
sakamino <shotapekka@gmail.com>
2023-02-24 05:55:25 +0900

    UI部分の統合テストは書き方が分からず、現時点では未完成。いずれ完成させる。日誌の画像投稿機能を作成開始

--- spec/requests/micropost_request_spec.rb
+++ spec/requests/micropost_request_spec.rb
@@ -1,32 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe "Microposts", type: :request do
-  describe "Microposts#create" do
+  describe "投稿作成時" do
     let(:micropost) { FactoryBot.attributes_for(:micropost) }
     let(:post_request) { post microposts_path, params: { micropost: micropost } }
 
-    context "when not logged in" do
-      it "doesn't change Micropost's count" do
+    context "非ログイン時" do
+      it "投稿数が変わらない" do
         expect { post_request }.to change(Micropost, :count).by(0)
       end
 
-      it "redirects to login_url" do
+      it "ログイン画面に移行" do
         expect(post_request).to redirect_to login_url
       end
     end
   end
 
-  describe "Microposts#destroy" do
+  describe "投稿削除時" do
     let!(:micropost) { FactoryBot.create(:micropost) }
     let(:delete_request) { delete micropost_path(micropost) }
 
-    context "when not logged in" do
-      it "doesn't change Micropost's count" do
+    context "非ログイン時" do
+      it "投稿数は変わらない" do
         expect { delete_request }.to change(Micropost, :count).by(0)
       end
 
-      it "redirects to login_url" do
+      it "ログイン画面へ移行" do
         expect(delete_request).to redirect_to login_url
+      end
+    end
+
+    context "ログイン済みユーザが他ユーザの投稿を削除しようとした時" do
+      let(:user) { FactoryBot.create(:user) }
+
+      before { log_in_as(user) }
+
+      it "投稿数は変わらない" do
+        expect { delete_request }.to change(Micropost, :count).by(0)
+      end
+      it "ログイン画面へ移行" do
+        expect(delete_request).to redirect_to root_url
       end
     end
   end
