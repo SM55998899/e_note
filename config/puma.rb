21e87b2548bdf90fddc2e47b4911ee7a1339fb12
sakamino <shotapekka@gmail.com>
2023-03-08 07:21:51 +0900

    circleCIファイルの調整

--- config/puma.rb
+++ config/puma.rb
@@ -3,7 +3,7 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 #port        ENV.fetch("PORT") { 3000 }
-bind "unix:///var/www/test_app/tmp/sockets/puma.sock"
+bind "unix:///var/www/e_note/tmp/sockets/puma.sock"
 environment ENV.fetch("RAILS_ENV") { "development" }
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 workers ENV.fetch("WEB_CONCURRENCY") { 4 }
