694258fbd5cf38c958cdda98b56d2dccf824e4fd
sakamino <shotapekka@gmail.com>
2023-03-20 23:31:31 +0900

    単語ページの装飾

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,10 @@ Bundler.require(*Rails.groups)
 
 module ENote
   class Application < Rails::Application
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.time_zone = "Asia/Tokyo"
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
