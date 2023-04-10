09659c611f4391e21a56541e22e5e1e99f204890
sakamino <shotapekka@gmail.com>
2023-04-10 16:18:11 +0900

    メイラーの修正7。rspec見直し前

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,7 +102,6 @@ Rails.application.configure do
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  :enable_starttls_auto => true,
   :address => "smtp.gmail.com",
   :port => 587,
   :domain => 'smtp.gmail.com',
