b85eb538fc7bfb2a2d75a436518de2a28e1808ee
sakamino <shotapekka@gmail.com>
2023-04-10 16:40:44 +0900

    メイラーの修正9。rspec見直し前

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,12 +102,12 @@ Rails.application.configure do
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
+  :enable_starttls_auto => true,
   :address => "smtp.gmail.com",
   :port => 587,
   :domain => 'smtp.gmail.com',
   :user_name => ENV['ACTION_MAILER_USER'],
   :password => ENV['ACTION_MAILER_KEY'],
-  :authentication => 'login',
-  :openssl_verify_mode  => 'none'
+  :authentication => 'login'
 }
 end
